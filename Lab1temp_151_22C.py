0dd1b35b66e78dbff14521d4980b8127e88d50ef
CerikNuells <112608445+CerikNuells@users.noreply.github.com>
2022-09-01 01:05:16 -0500

    Add files via upload

---
+++ Lab1temp_151_22C.py
@@ -0,0 +1,96 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# ## MATH 151 Lab  1
+
+# Put team members' names and section number here.
+
+# In[ ]:
+
+
+from sympy import *
+from sympy.plotting import (plot,plot_parametric)
+
+
+# ### Question 1
+
+# #### 1a
+
+# In[ ]:
+
+
+#start code here
+
+
+# #### 1b
+
+# In[ ]:
+
+
+#start code here
+
+
+# ### Question 2
+
+# #### 2a
+
+# In[ ]:
+
+
+#start code here
+
+
+# #### 2b
+
+# In[ ]:
+
+
+#start code here
+
+
+# #### 2c
+
+# In[ ]:
+
+
+#start code here
+
+
+# #### 2d
+
+# In[ ]:
+
+
+#start code here
+
+
+# ### Question 3
+
+# In[ ]:
+
+
+#start code here
+
+
+# #### 3b
+
+# In[8]:
+
+
+from sympy import *
+from sympy.plotting import (plot,plot_parametric)
+
+v,a,x,h = symbols('v, a, x, h')
+v= 24
+a = 54.2
+x = 15
+y = (((-16)/((v ** 2)*(cos(a)**2))) * (x**2)) + (tan(a)) * x 
+h = 10 - y
+print('The exact answer is',h)
+
+
+# In[ ]:
+
+
+
+
